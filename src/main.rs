836e0097e53796153bcf60a24832afd216bca39d
zimingYS <1563238589@qq.com>
2024-11-27 22:33:03 +0800

    为窗口增加State结构体以及对应的实现方法

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use wgpu::{Features, Instance, Limits, PowerPreference, SurfaceConfiguration, TextureUsages};
+use wgpu::*;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -6,9 +6,9 @@ use winit::{
 };
 
 struct State {
-    surface: wgpu::Surface,
-    device: wgpu::Device,
-    queue: wgpu::Queue,
+    surface: Surface,
+    device: Device,
+    queue: Queue,
     config: SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
 }
@@ -79,7 +79,7 @@ impl State {
 
             //present_mode 使用 wgpu::PresentMode 枚举值来确定应如何将 surface 同步到显示器上
             //使用FIFO将显示速率限制为显示器的帧速率（仅Fifo可适配移动设备）
-            present_mode: wgpu::PresentMode::Fifo,
+            present_mode: PresentMode::Fifo,
         };
         surface.configure(&device,&config);
 
@@ -115,11 +115,59 @@ impl State {
     }
 
     fn update(&mut self) {
-        todo!()
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        todo!()
+        //get_current_texture 函数会等待 surface 提供一个新的 SurfaceTexture 以用于渲染
+        let output = self.surface.get_current_texture()?;
+
+        //创建了一个使用默认配置的 TextureView 用于控制渲染代码与纹理之间的交互
+        let view = output.texture.create_view(&TextureViewDescriptor::default());
+
+        //创建一个 CommandEncoder 来创建实际发送到 GPU 上的命令
+        //使用 encoder 充当命令缓冲区
+        let mut encoder = self.device.create_command_encoder(
+            &CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            }
+        );
+
+        //使用 encoder 来创建 RenderPass 以执行清屏
+        //由于 begin_render_pass() 是以可变方式借用了 encoder , 因此在我们释放这个可变的借用之前，我们都不能调用 encoder.finish()
+        //所以需要释放 encoder 上的可变借用，从而使得我们能使用 finish()
+        {
+            //使用作用域以释放内存
+            let _render_pass = encoder.begin_render_pass(&RenderPassDescriptor{
+                //开启渲染通道
+                label: Some("Render Pass"),
+                //color_attachments 定义颜色附件,用于存储颜色数据的纹理
+                color_attachments: &[RenderPassColorAttachment{
+                    //iew 变量作为颜色附件的视图
+                    view: &view,
+                    //resolve_target 用于指定颜色附件的解析目标
+                    resolve_target: None,
+                    //Operations 用于描述颜色附件的操作
+                    ops: Operations{
+                        //load 字段来指定颜色附件的加载,其中Clear用来清除颜色附件
+                        load: LoadOp::Clear(wgpu::Color{
+                            r : 0.1,
+                            g : 0.2,
+                            b : 0.3,
+                            a : 1.0  //此处为不透度
+                        }),
+                        //store 用于指定颜色附件的存储操作
+                        store: true,
+                    }
+                }],
+                //用于定义深度和模板附件,此处没有定义深度和模板附件，所以置为None
+                depth_stencil_attachment: None,
+            });
+        }
+        // submit 方法能传入任何实现了 IntoIter 的参数
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
 }
 
@@ -134,31 +182,55 @@ fn main() {
         .unwrap();
     let mut state = pollster::block_on(State::new(&window));
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                    ..
-                },
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            //此处用于窗口改变大小后重绘
-            WindowEvent::Resized(physical_size) => {
-                state.resize(*physical_size);
+    //初始化窗口大小
+    state.resize(winit::dpi::PhysicalSize::new(800, 600));
+
+    event_loop.run(move |event, _, control_flow|
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
             }
-            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                // new_inner_size 是 &&mut 类型，因此需要解引用两次
-                state.resize(**new_inner_size);
+            if window_id == window.id() => match event {
+                WindowEvent::CloseRequested | WindowEvent::KeyboardInput {
+                    input: KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+
+                //此处用于窗口改变大小后重绘
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(*physical_size);
+                }
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    // new_inner_size 是 &&mut 类型，因此需要解引用两次
+                    state.resize(**new_inner_size);
+                },
+
+                _ => {}
+            },
+            //使用事件更新调用render方法
+            Event::RedrawRequested(window_id ) if window_id == window.id() => {
+                //调用state.upadte 以更新state
+                state.update();
+                match state.render() {
+                    Ok(_) => {},
+                    //如果发生上下文丢失，则重新配置surface
+                    Err(SurfaceError::Lost) => state.resize(state.size),
+                    //内存不足时退出
+                    Err(SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    //若出现其他错误则应在下一帧处理
+                    //打印输出错误
+                    Err(e) => eprintln!("{:?}",e),
+                }
+            },
+            Event::MainEventsCleared => {
+                // 除非手动请求，否则 RedrawRequested 只会触发一次
+                window.request_redraw();
             },
             _ => {}
-        },
-        _ => {}
     });
 }
